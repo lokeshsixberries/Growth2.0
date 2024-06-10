6c26e15f22e2eec1ac53e071bd6207b624b94891
Lokesh Sharma <104984979+lokeshsixberries@users.noreply.github.com>
2024-06-10 15:47:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(errorHandler);
 
 
 // Start the server
-const PORT = 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
